30d1a44e0509e58829a83341e07048ab7673b22d
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-21 20:59:25 +0800

    Added new class note1

--- WinApp20220921/WinApp20220921/Form1.Designer.cs
+++ WinApp20220921/WinApp20220921/Form1.Designer.cs
@@ -33,25 +33,32 @@ namespace WinApp20220921
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmValue = new System.Windows.Forms.TextBox();
+            this.kgValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(326, 66);
+            this.button1.Location = new System.Drawing.Point(435, 65);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(94, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "產生300";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(407, 66);
+            this.button2.Location = new System.Drawing.Point(535, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(94, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "計算適當體重";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -59,30 +66,104 @@ namespace WinApp20220921
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 16F);
-            this.label1.Location = new System.Drawing.Point(136, 64);
+            this.label1.Location = new System.Drawing.Point(431, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.Size = new System.Drawing.Size(162, 22);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "輸入身高(公分):";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(138, 102);
+            this.textBox1.Location = new System.Drawing.Point(599, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F);
+            this.label2.Location = new System.Drawing.Point(23, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "請輸入您的身高(cm):";
+            // 
+            // cmValue
+            // 
+            this.cmValue.Font = new System.Drawing.Font("新細明體", 15F);
+            this.cmValue.Location = new System.Drawing.Point(257, 24);
+            this.cmValue.Name = "cmValue";
+            this.cmValue.Size = new System.Drawing.Size(100, 31);
+            this.cmValue.TabIndex = 5;
+            // 
+            // kgValue
+            // 
+            this.kgValue.Font = new System.Drawing.Font("新細明體", 15F);
+            this.kgValue.Location = new System.Drawing.Point(257, 62);
+            this.kgValue.Name = "kgValue";
+            this.kgValue.Size = new System.Drawing.Size(100, 31);
+            this.kgValue.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 18F);
+            this.label3.Location = new System.Drawing.Point(23, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "請輸入您的體重(kg):";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 16F);
+            this.button3.Location = new System.Drawing.Point(266, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "計算";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 18F);
+            this.label4.Location = new System.Drawing.Point(48, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "BMI:";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("新細明體", 18F);
+            this.Result.Location = new System.Drawing.Point(113, 132);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(22, 24);
+            this.Result.TabIndex = 10;
+            this.Result.Text = "  ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.kgValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmValue);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BMI 計算器";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +175,13 @@ namespace WinApp20220921
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cmValue;
+        private System.Windows.Forms.TextBox kgValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Result;
     }
 }
 
--- WinApp20220921/WinApp20220921/Form1.cs
+++ WinApp20220921/WinApp20220921/Form1.cs
@@ -18,14 +18,20 @@ namespace WinApp20220921
         }
 
         /* 	變數的命名規則
-	
-	    為甚麼要設定宣告變數: 為了記住記憶體位置	
-	
-	    1. 首自只能使用文字或底線(文字不分中英文，只要是文字即可!)
-	    2. 第二字後，可用文字、數字及底線。
-	    3. 不得使用關鍵字。
+	            為甚麼要設定宣告變數? ANS:為了記住記憶體位置!!	
+	            1. 首自只能使用文字或底線(文字不分中英文，只要是文字即可!)
+	            2. 第二字後，可用文字、數字及底線。
+	            3. 不得使用關鍵字。
+            
+            駱駝命名法(變數方法):
+               https://zh.m.wikipedia.org/zh-tw/%E9%A7%9D%E5%B3%B0%E5%BC%8F%E5%A4%A7%E5%B0%8F%E5%AF%AB
+            巴斯卡命名法(類別物件):
+                Visual Studio 使用其命名法! 
+                https://www.easyatm.com.tw/wiki/%E5%B8%95%E6%96%AF%E5%8D%A1%E5%91%BD%E5%90%8D%E6%B3%95
 
         */
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,16 +41,29 @@ namespace WinApp20220921
 	        */
 
             int aa = 100;
-            aa = aa + 200;
+            aa += 200;
             label1.Text = aa.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string aa = textBox1.Text;
-            int bb = int.Parse(aa);
-            bb = bb + 200;
-            label1.Text = bb.ToString();
+            string inputText = textBox1.Text;       //使用者輸入身高
+            int userHeight = int.Parse(inputText);
+            int stdWeight = userHeight - 110;
+            label1.Text = "標準體重: " + stdWeight.ToString() + "公斤";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string cmV = cmValue.Text;      //使用這輸入身高
+            float m = float.Parse(cmV);     //輸入轉換成浮點數
+            m = (float)(m / 100);           //身高轉換成公尺
+
+            string kgV = kgValue.Text;      //使用者輸入體重
+            float kg = float.Parse(kgV);    //輸入轉換成浮點數
+
+            float BMI = kg / (m * m);       //公式計算BMI值
+            Result.Text = BMI.ToString();   //將結果輸出到label
         }
     }
 }
