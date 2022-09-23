a9ddf2bced7fcdf3fae3cc38c1c2fb5cf0059cd1
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-23 21:20:08 +0800

    switch case, and , if else

--- WinApp20220923/WinApp20220923/Form1.Designer.cs
+++ WinApp20220923/WinApp20220923/Form1.Designer.cs
@@ -33,15 +33,20 @@ namespace WinApp20220923
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(155, 64);
+            this.button1.Location = new System.Drawing.Point(155, 66);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "檢查";
+            this.button1.Text = "檢查及格";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -55,9 +60,10 @@ namespace WinApp20220923
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 103);
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F);
+            this.label1.Location = new System.Drawing.Point(47, 108);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.Size = new System.Drawing.Size(58, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "結果";
             // 
@@ -68,13 +74,64 @@ namespace WinApp20220923
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "輸入成績";
+            this.label2.Text = "輸入數值";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "檢查數值";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "檢查等級";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(155, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "餐號選擇";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 48);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "1 : 豬排 \r\n2 : 雞排\r\n3 : 牛排\r\n4 : 羊排";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -92,6 +149,11 @@ namespace WinApp20220923
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- WinApp20220923/WinApp20220923/Form1.cs
+++ WinApp20220923/WinApp20220923/Form1.cs
@@ -31,5 +31,90 @@ namespace WinApp20220923
                 label1.ForeColor = Color.Red;
             }
         }
+        /*  巢狀 IF
+           if()
+           {
+               if()
+                   {}
+           }
+         */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int inputNum = int.Parse(textBox1.Text);
+            if (inputNum < 0)
+            {
+                label1.Text = "負數";
+            }
+            else if (inputNum < 10)
+            {
+                label1.Text = "個位數";
+            }
+            else if (inputNum < 100)
+            {
+                label1.Text = "2位數";
+            }
+            else if (inputNum < 1000)
+            {
+                label1.Text = "3位數";
+            }
+            else
+            {
+                label1.Text = "4位數以上";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int inputNum = int.Parse(textBox1.Text);
+            if (inputNum < 60)
+            {
+                label1.Text = "戊等";
+            }
+            else if (inputNum < 70)
+            {
+                label1.Text = "丁等";
+            }
+            else if (inputNum < 80)
+            {
+                label1.Text = "丙等";
+            }
+            else if (inputNum < 90)
+            {
+                label1.Text = "乙等";
+            }
+            else
+            {
+                label1.Text = "甲等";
+            }
+
+        }
+
+
+
+        /* 選項變數 switch
+            
+            switch 
+            {
+                case [選項]: [處理敘述];
+                break;
+            }
+
+        */
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int choice = int.Parse(textBox2.Text);
+            string s = "";
+            switch (choice)
+            {
+                case 1: s = "豬排"; break;
+                case 2: s = "雞排"; break;
+                case 3: s = "牛排"; break;
+                case 4: s = "羊排"; break;
+                case 5:                     // 選四就是選五
+                default: s = "只有1-4，不要亂選!"; break;
+            }
+            label1.Text = s;
+        }
     }
 }
