a19e26aae300278325aea11812eef7a5cf8cbb43
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-26 20:43:22 +0800

    Added sum odd & sum even example

--- WinApp20220926/WinApp20220926/Form1.Designer.cs
+++ WinApp20220926/WinApp20220926/Form1.Designer.cs
@@ -31,32 +31,68 @@ namespace WinApp20220926
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 88);
+            this.textBox1.Location = new System.Drawing.Point(32, 38);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(259, 245);
+            this.textBox1.Size = new System.Drawing.Size(343, 295);
             this.textBox1.TabIndex = 0;
-            this.textBox1.WordWrap = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(297, 88);
+            this.button1.Location = new System.Drawing.Point(381, 38);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "文字記本範例";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "文字進階顯示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(382, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "For 迴圈範例";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(382, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -70,6 +106,9 @@ namespace WinApp20220926
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WinApp20220926/WinApp20220926/Form1.cs
+++ WinApp20220926/WinApp20220926/Form1.cs
@@ -16,5 +16,79 @@ namespace WinApp20220926
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = "Mom says:\"It's time to wake up!\"\r\nMe:\"Okay!!\"";
+            textBox1.Text = a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string a = "巨匠電腦桃園認證中心桃園市政府";
+
+            result = result + "字串長度: " + a.Length + "\r\n";
+            result = result + "子字串1: " + a.Substring(10) + "\r\n";
+            result = result + "子字串2: " + a.Substring(4, 2) + "\r\n";
+
+            result = result + "從前搜尋位置1: " + a.IndexOf("桃園") + "\r\n";
+            result = result + "從前搜尋位置2: " + a.IndexOf("桃園", 5) + "\r\n";
+
+            result = result + "從後搜尋位置1: " + a.LastIndexOf("桃園") + "\r\n";
+            result = result + "從後搜尋位置2: " + a.LastIndexOf("桃園", 6) + "\r\n";
+
+            result = result + "取代: " + a.Replace("桃園", "台北") + "\r\n";
+
+            result = result + "插入: " + a.Insert(10, "新竹") + "\r\n";
+
+            // 分割 
+            string b = "AA,BB:CC.DD,EE:FF";
+            string[] bb = b.Split(',', ':', '.');
+            for (int i = 0; i < bb.Length; i++)
+                result += bb[i] + "\r\n";
+
+            textBox1.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            int i = 1;
+
+            for ( ; i <=10; i +=2)
+            {
+                result += "執行第" + i.ToString() + "次，巨匠電腦\r\n";
+            }
+
+            result += "執行第" + i.ToString() + "次";
+
+            textBox1.Text = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int startNum = 300;
+            int sumNum = 0;
+            int sumNum偶數 = 0;
+            int sumNum奇數 = 0;
+
+            for (; startNum <= 1500; startNum ++)
+            {
+                sumNum += startNum;
+                if (startNum % 2 ==0)
+                {
+                    sumNum偶數 += startNum;
+                }
+                else
+                {
+                    sumNum奇數 += startNum;
+                }
+            }
+
+            textBox1.Text = "總和: \t" + sumNum.ToString() + "\r\n" +
+                            "奇數和: \t" + sumNum奇數.ToString() + "\r\n" +
+                            "偶數和: \t" + sumNum偶數.ToString();
+        }
     }
 }
