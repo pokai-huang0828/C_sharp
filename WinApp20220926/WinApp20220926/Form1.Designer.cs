de785fb3ed3d83f45a1d29f72310b4cfd1204cbf
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-26 21:35:23 +0800

    Added class code

--- WinApp20220926/WinApp20220926/Form1.Designer.cs
+++ WinApp20220926/WinApp20220926/Form1.Designer.cs
@@ -34,20 +34,23 @@ namespace WinApp20220926
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 38);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(343, 295);
+            this.textBox1.Size = new System.Drawing.Size(493, 426);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(381, 38);
+            this.button1.Location = new System.Drawing.Point(523, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 1;
@@ -57,7 +60,7 @@ namespace WinApp20220926
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(382, 77);
+            this.button2.Location = new System.Drawing.Point(523, 51);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 2;
@@ -67,7 +70,7 @@ namespace WinApp20220926
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(382, 115);
+            this.button3.Location = new System.Drawing.Point(523, 89);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(104, 23);
             this.button3.TabIndex = 3;
@@ -77,19 +80,52 @@ namespace WinApp20220926
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(382, 156);
+            this.button4.Location = new System.Drawing.Point(523, 130);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(104, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "ForSumNumEX";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(523, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "WhileSumNumEX";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(523, 211);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "3的n次方值";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(523, 250);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Do 迴圈";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -109,6 +145,9 @@ namespace WinApp20220926
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- WinApp20220926/WinApp20220926/Form1.cs
+++ WinApp20220926/WinApp20220926/Form1.cs
@@ -90,5 +90,72 @@ namespace WinApp20220926
                             "奇數和: \t" + sumNum奇數.ToString() + "\r\n" +
                             "偶數和: \t" + sumNum偶數.ToString();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = 300;
+            int sumNum = 0;
+            int sumNum偶數 = 0;
+            int sumNum奇數 = 0;
+
+            while (i <= 1500)
+            {
+                sumNum += i;
+                if (i % 2 == 0)
+                {
+                    sumNum偶數 += i;
+                    i++;
+                }
+                else
+                {
+                    sumNum奇數 += i;
+                    i++;
+                }
+            }
+
+            textBox1.Text = "總和: \t" + sumNum.ToString() + "\r\n" +
+                            "奇數和: \t" + sumNum奇數.ToString() + "\r\n" +
+                            "偶數和: \t" + sumNum偶數.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            long total = 1; //uint -> unsign 不標號
+            int count = 0;
+            string result = "";
+
+            while (total <= 2000000000)
+            {
+                total *= 3;
+                count += 1;
+                result += "3 的" + count + " 次方的值: " + total + "\r\n";
+            }
+
+            result += "3 的" + count + " 次方的值: " + total;
+            textBox1.Text = result;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            long total = 1; //uint -> unsign 不標號
+            int count = 0;
+            string result = "";
+            // 只少會做一次
+            do
+            {
+                total *= 3;
+                count += 1;
+                result += "3 的" + count + " 次方的值: " + total + "\r\n";
+
+            } while (total <= 2000000000);
+
+            textBox1.Text = result;
+        }
+
+        /* 複製一篇文章
+         * 
+         * 
+         
+         */
     }
 }
