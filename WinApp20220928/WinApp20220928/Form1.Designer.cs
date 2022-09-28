6ccce246362e448af6421cf9cb002b935ec87265
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-28 21:34:49 +0800

    Added class code & homework

--- WinApp20220928/WinApp20220928/Form1.Designer.cs
+++ WinApp20220928/WinApp20220928/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace WinApp20220928
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -118,11 +119,22 @@ namespace WinApp20220928
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(647, 215);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(141, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "格式化";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -148,6 +160,7 @@ namespace WinApp20220928
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- WinApp20220928/WinApp20220928/Form1.cs
+++ WinApp20220928/WinApp20220928/Form1.cs
@@ -118,7 +118,8 @@ namespace WinApp20220928
             {
                 for (j = 1; j <= 9; j++)
                 {
-                    result += i + "*" + j + "=" + i * j + "\t";
+                    //result += i + "*" + j + "=" + (i * j).ToString("00") + "  ";
+                    result += string.Format("{0}*{1}={2:00}  ", i, j, i * j);
                     if (i == 7 && j == 6)
                     {
                         isBreak = true;
@@ -129,6 +130,53 @@ namespace WinApp20220928
                 result += "\r\n";
             }
             textBox1.Text = result;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string a = "巨匠電腦";
+            int b = 12345678;
+            double c = 56.789;
+            DateTime d = DateTime.Now;
+
+            string reault = "";
+
+            reault += b.ToString("c") + "\r\n";
+            reault += c.ToString("p") + "\r\n";
+            reault += c.ToString("#.0%") + "\r\n";
+            reault += d.ToLongDateString() + "\r\n";
+            reault += d.ToShortDateString() + "\r\n";
+            reault += d.ToLongTimeString() + "\r\n";
+            reault += d.ToShortTimeString() + "\r\n";
+            reault += d.ToString("D") + "\r\n";
+            reault += d.ToString("d") + "\r\n";
+            reault += d.ToString("yyyy-MM-dd dddd HH:mm:ss") + "\r\n";
+
+            reault += string.Format("A的值: {0}, B的值: {1:C}, C的值: {2:P}, D的值 {3:D}, B的值: {1:#.0}", a, b, c, d);
+
+
+            textBox1.Text = reault;
+
+            /*
+                1. 2-1000 質數
+                2. 100-1000 中的 {水仙花} 數 "阿姆斯壯數"
+                3.*
+                  **
+                  ***
+                  ****
+                  *****
+                4.    *
+                     **
+                    ***
+                   ****
+                  *****
+                5.     *
+                      ***
+                     *****
+                    *******
+                   *********
+              
+            */
         }
     }
 }
