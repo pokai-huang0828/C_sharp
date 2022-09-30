d66f4a8b692e2de6cdb482d2a8911fa95bc18749
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-30 20:34:02 +0800

    Added Class code

--- WinApp20220930/WinApp20220930/Form1.Designer.cs
+++ WinApp20220930/WinApp20220930/Form1.Designer.cs
@@ -31,6 +31,10 @@ namespace WinApp20220930
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -44,23 +48,67 @@ namespace WinApp20220930
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(521, 13);
+            this.button1.Location = new System.Drawing.Point(520, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(149, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "基礎陣列Array";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(520, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "陣列ArrayWithForeach";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(520, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "一維陣列";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(520, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "陣列ArrayWith巢狀";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(520, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "二維陣列";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Array Test";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +118,10 @@ namespace WinApp20220930
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WinApp20220930/WinApp20220930/Form1.cs
+++ WinApp20220930/WinApp20220930/Form1.cs
@@ -16,14 +16,83 @@ namespace WinApp20220930
         {
             InitializeComponent();
         }
-
-        /*
-        
-        */
+        /* ----------------一維 Array 陣列---------------------- */
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //int[] xx;           // 宣告
+            //xx = new int[5];    // 實體化
 
+            int[] xx = new int[5];
+
+            xx[0] = 90;
+            xx[1] = 80;
+            xx[2] = 70;
+            xx[3] = 60;
+            xx[4] = 50;
+
+            string result = "";
+            /*
+            result += xx[0] + "\r\n";
+            result += xx[1] + "\r\n";
+            result += xx[2] + "\r\n";
+            result += xx[3] + "\r\n";
+            result += xx[4] + "\r\n";
+            */
+            for (int i = 0; i < 5; i++)
+            {
+                result += xx[i] + "\r\n";
+            }
+
+            textBox1.Text = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //int[] xx = new int[] { 90, 80, 70, 60, 50 };
+            int[] xx = { 90, 80, 70, 60, 50, 40, 30, 20, 10 };
+
+            string result = "";
+            int total = 0;
+
+            foreach (int aa in xx)
+            {
+                total += aa;
+                result += aa + "\r\n";
+            }
+
+            result += "總和: " + total;
+            textBox1.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[,] xx = new int[4, 5];
+
+            xx[0, 0] = 90;
+            xx[0, 2] = 80;
+            xx[0, 3] = 70;
+            xx[1, 0] = 60;
+            xx[1, 1] = 50;
+            xx[1, 3] = 40;
+            xx[2, 1] = 30;
+            xx[2, 2] = 20;
+            xx[2, 3] = 10;
+
+            string result = "";
+
+            result += xx.Length + "\r\n";
+            result += "第 0 維度: " + xx.GetLength(0) + "\r\n";
+            result += "第 1 維度: " + xx.GetLength(1) + "\r\n";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    result += xx[i, j] + "\t";
+                result += "\r\n";
+            }
+
+            textBox1.Text = result;
         }
     }
 }
